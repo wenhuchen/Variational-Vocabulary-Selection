51ff40dea35c1369a1929ffbe919ce5f961b71aa
wenhu chen <wenhuchen@cs.ucsb.edu>
2019-03-11 22:17:38 -0700

    add mkdir folder

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import time
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default="word_cnn",
@@ -152,6 +153,10 @@ with tf.Session() as sess:
         model_folder = "{}_models/{}_{}_l1".format(args.dataset, args.id, args.emb_size)
     else:
         model_folder = "{}_models/{}_{}".format(args.dataset, args.id, args.emb_size)
+	
+	if not os.path.exists(model_folder):
+		os.makedirs(model_folder)
+
     model_name = os.path.join(model_folder, "model.ckpt")
 
     if tf.train.checkpoint_exists(model_folder):
